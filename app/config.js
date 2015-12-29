1c1bbc7651bc6107d359dab419b640c60ebee0d5
Tim Boyce <timothy.boyce@mmgins.com>
2015-12-29 17:42:42 +0000

    remove typescript

--- app/config.js
+++ app/config.js
@@ -18,7 +18,6 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
-    "typescript": "npm:typescript@1.7.5",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
